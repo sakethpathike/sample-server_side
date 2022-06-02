d3e8c91f3bc70631372d8f8e7e21402649498bb1
SakethPathike <sakethpathike@gmail.com>
2022-06-02 20:39:46 +0530

    Made Few Changes In Order To Deploy

--- src/clientStuff/MongoDBClient.kt
+++ src/clientStuff/MongoDBClient.kt
@@ -1,15 +1,11 @@
 package com.saketh.sample.clientStuff
 
 import com.mongodb.client.MongoCollection
-import com.saketh.sample.COLLECTION_NAME
-import com.saketh.sample.DATABASE_NAME
-import com.saketh.sample.PASSWORD
-import com.saketh.sample.USER_NAME
 import org.bson.Document
 import org.litote.kmongo.KMongo
 
 fun mongoClient(): MongoCollection<Document> {
-    return KMongo.createClient("mongodb+srv://$USER_NAME:$PASSWORD@cluster0.2wr7r7n.mongodb.net/?retryWrites=true&w=majority")
-        .getDatabase(DATABASE_NAME)
-        .getCollection(COLLECTION_NAME)
+    return KMongo.createClient("mongodb+srv://${System.getenv("USER_NAME")}:${System.getenv("PASSWORD")}@cluster0.2wr7r7n.mongodb.net/?retryWrites=true&w=majority")
+        .getDatabase(System.getenv("DATABASE_NAME"))
+        .getCollection(System.getenv("COLLECTION_NAME"))
 }
