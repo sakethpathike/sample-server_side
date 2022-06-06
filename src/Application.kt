c684cfec20cf6046bf2da4cdfbefb1448d4aa646
SakethPathike <sakethpathike@gmail.com>
2022-06-06 19:23:38 +0530

    Made Few Changes In Code

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,10 @@
 package com.saketh.sample
 
-import com.mongodb.client.MongoCollection
 import com.saketh.sample.songDetails.*
 import io.ktor.application.*
 import io.ktor.routing.*
 import io.ktor.gson.*
 import io.ktor.features.*
-import org.bson.Document
-import org.litote.kmongo.KMongo
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
